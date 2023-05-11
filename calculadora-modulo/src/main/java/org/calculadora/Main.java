e420b920cf670040bd820c8b037ea8136cbfa114
leona <leonardo.azevdo@gmail.com>
2023-05-11 20:35:50 -0300

    refactoring main class

--- calculadora-modulo/src/main/java/org/calculadora/Main.java
+++ calculadora-modulo/src/main/java/org/calculadora/Main.java
@@ -1,11 +1,7 @@
 package org.calculadora;
 
-import org.calculadora.calculadora.Calculadora;
-
 public class Main {
     public static void main(String[] args) {
 
-        Calculadora calculadora = new Calculadora();
-        System.out.println(calculadora.calcular(4,2,"/"));
     }
 }
