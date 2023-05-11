4fb36082df6b71172b80b94f3dbba5a05044eb29
leona <leonardo.azevdo@gmail.com>
2023-05-11 19:39:52 -0300

    refatorando

--- calculadora-modulo/src/main/java/org/calculadora/calculadora/Calculadora.java
+++ calculadora-modulo/src/main/java/org/calculadora/calculadora/Calculadora.java
@@ -1,7 +1,5 @@
 package org.calculadora.calculadora;
 
-import java.math.BigDecimal;
-
 public class Calculadora {
 
     private static final String OPERADOR_SOMA = "+";
@@ -9,7 +7,7 @@ public class Calculadora {
     private static final String OPERADOR_MULTIPLICACAO = "*";
     private static final String OPERADOR_DIVISAO = "/";
 
-    public Integer calcular(int variavel1, int variavel2, String operador) {
+    public static Integer calcular(int variavel1, int variavel2, String operador) {
 
         if(OPERADOR_SOMA.equals(operador)) {
             return variavel1 + variavel2;
